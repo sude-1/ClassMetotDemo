37f59bb279c7f5bd276f4425f2da70ffcdcc5fd0
Sude Karanlık <sudekaranlikk@hotmail.com>
2021-03-20 17:51:41 +0300

    16 Ocak 2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            musteri.Adi = "Sude";
+            musteri.Soyadi = "Karanlık";
+            musteri.Id = 3;
+            musteri.TelNo = 765689;
+            Console.WriteLine(musteri.Adi +" "+ musteri.Soyadi+ " " +"Müşteri Eklendi");
+        }
+
+        public void Delete(Musteri musteri1)
+        {
+            Console.WriteLine(musteri1.Adi +" "+musteri1.Soyadi + " " + "Müşteri Silindi");
+        }
+
+        public void Listele(List<Musteri> musteris)
+        {
+            foreach (var musterim in musteris)
+            {
+                Console.WriteLine(musterim.Adi + " " + musterim.Soyadi + " " + "Müşteriler Listelendi");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Kaan";
+            musteri1.Soyadi = "Durmaz";
+            musteri1.TelNo = 22346;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Tolga";
+            musteri2.Soyadi = "Gündüz";
+            musteri2.TelNo = 78609;
+
+            Musteri musteriekle = new Musteri();
+            List<Musteri> musteris = new List<Musteri>() { musteri1, musteri2, musteriekle };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteriekle);
+            musteriManager.Delete(musteri1);
+            musteriManager.Listele(musteris);
+
+        }
+    }
+}
